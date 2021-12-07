55df2a49c527120f0980628804438625b12d417c
ketangote <ketan.gote@metamagic.in>
2021-12-07 15:20:04 +0530

    Changes: Prod Service..

--- src/main/java/com/product/adapter/controller/v1/ProductController.java
+++ src/main/java/com/product/adapter/controller/v1/ProductController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(V1)
+@CrossOrigin("*")
 public class ProductController {
 
 	private static final Logger log = (Logger) LoggerFactory.getLogger(ProductController.class);
